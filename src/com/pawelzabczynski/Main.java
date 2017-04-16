899d711ea3531a46cf360c2649a91f4e61622a0d
sezerp <zabczynskipawe@gmail.com>
2017-04-16 20:08:33 +0100

    validation debi card java class

---
+++ src/com/pawelzabczynski/Main.java
@@ -0,0 +1,16 @@
+package com.pawelzabczynski;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        debiCardValid card = new debiCardValid();
+
+        if(card.isValid(4448316251826725L))System.out.println("Is valid");else System.out.println("Is invalid");
+
+    }
+
+
+
+
+}
